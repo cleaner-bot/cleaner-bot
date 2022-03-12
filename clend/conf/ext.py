b2f94047a84b136b314a9fc97f7d82e733023b81
Leo Developer <git@leodev.xyz>
2022-03-12 14:55:25 +0100

    add config sync between dashboard and bot

--- clend/conf/ext.py
+++ clend/conf/ext.py
@@ -1,5 +1,6 @@
 import asyncio
 import typing
+import json
 import logging
 
 import hikari
@@ -8,6 +9,7 @@ from cleaner_conf import Config, config, Entitlements, entitlements
 
 from ..bot import TheCleaner
 from ..shared.event import IGuildSettingsAvailable
+from ..shared.sub import listen as pubsub_listen, Message
 
 
 logger = logging.getLogger(__name__)
@@ -33,12 +35,19 @@ class ConfigExtension:
 
         self._guilds = {}
         self._races = {}
+        self.task = None
 
     def on_load(self):
         for guild_id in self.bot.bot.cache.get_guilds_view().keys():
             if guild_id not in self._guilds:
                 logger.debug(f"scheduled config:fetch_guild({guild_id})")
                 asyncio.create_task(self.fetch_guild(guild_id))
+
+        self.task = asyncio.create_task(self.updated())
+
+    def on_unload(self):
+        if self.task is not None:
+            self.task.cancel()
 
     async def fetch_guild(self, guild_id: int):
         event = self._races.get(guild_id, None)
@@ -108,3 +117,31 @@ class ConfigExtension:
     async def on_destroy_guild(self, event: hikari.GuildLeaveEvent):
         if event.guild_id in self._guilds:
             del self._guilds[event.guild_id]
+
+    async def updated(self):
+        while True:
+            try:
+                await self._updated()
+            except Exception as e:
+                logger.exception("Exception occured during updated", exc_info=e)
+
+    async def _updated(self):
+        pubsub = self.bot.database.pubsub()
+        await pubsub.subscribe("pubsub:config-update")
+        async for event in pubsub_listen(pubsub):
+            if not isinstance(event, Message):
+                continue
+
+            data = json.loads(event.data)
+            gd = self._guilds.get(data["guild_id"], None)
+            if gd is None:
+                continue
+
+            obj = gd.config if data["table"] == "config" else gd.entitlements
+            validators = config if data["table"] == "config" else entitlements
+
+            for name, value in data["changes"].items():
+                logger.debug(
+                    f"changed {data['table']}.{name} to {value!r} ({data['guild_id']})"
+                )
+                setattr(obj, name, validators[name].from_string(value))
---
+++ clend/shared/sub.py
@@ -0,0 +1,43 @@
+import typing
+import logging
+
+from coredis.pubsub import PubSub  # type: ignore
+
+
+logger = logging.getLogger(__name__)
+
+
+class Message(typing.NamedTuple):
+    data: str
+    channel: str
+    is_pattern: bool
+
+
+class Subscribe(typing.NamedTuple):
+    data: str
+    channel: str
+    is_pattern: bool
+
+
+class Unsubscribe(typing.NamedTuple):
+    data: str
+    channel: str
+    is_pattern: bool
+
+
+async def listen(
+    pubsub: PubSub, /, **kwargs
+) -> typing.AsyncGenerator[Message | Subscribe | Unsubscribe, None]:
+    while True:
+        message = await pubsub.get_message(**kwargs)
+        is_pattern = message["type"].startswith("p")
+        type = message["type"][1:] if is_pattern else message["type"]
+
+        if type == "subscribe":
+            yield Subscribe(message["data"], message["channel"], is_pattern)
+        elif type == "unsubscribe":
+            yield Unsubscribe(message["data"], message["channel"], is_pattern)
+        elif type == "message":
+            yield Message(message["data"], message["channel"], is_pattern)
+        else:
+            logger.warning(f"received unknown type on pubsub: {type} ({is_pattern})")
