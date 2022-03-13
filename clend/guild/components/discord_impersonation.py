1f50cac6e6d8ca4134faa71eeb7a2e8c4215e82c
Leo Developer <git@leodev.xyz>
2022-03-13 12:58:05 +0100

    move banned names into cleaner_data and add avatar detection

--- clend/guild/components/discord_impersonation.py
+++ clend/guild/components/discord_impersonation.py
@@ -1,41 +1,25 @@
 import hikari
-from cleaner_data.unicode import normalize_unicode
+from cleaner_data.name import is_name_blacklisted
+from cleaner_data.auto.avatars import data as avatar_blacklist
 
 from ..guild import CleanerGuild
 from ..helper import action_challenge
-
-
-impersonation_words = {
-    "developer",
-    "partners",
-    "exam",
-    "devbot",
-    "mod",
-    "support",
-    "intents",
-    "api",
-    "partner",
-    "message",
-    "moderator",
-    "academy",
-    "developers",
-    "hypesquad",
-    "moderators",
-}
 
 
 def on_member_create(event: hikari.MemberCreateEvent, guild: CleanerGuild):
     config = guild.get_config()
     if config is None or not config.overview_discordimpersonation_enabled:
         return
-    name = normalize_unicode(event.member.display_name)
-    words = name.split()
-    if not all(w in impersonation_words for w in words):
+    if (
+        not is_name_blacklisted(event.user.username)
+        or event.user.avatar_hash not in avatar_blacklist
+    ):
         return
 
     action = action_challenge(guild, event.member, "discord-impersonation")
     if action.can_role or action.can_timeout:
         action = action._replace(can_role=False, can_timeout=False)
+    # TODO: report name and avatar
     return (action,)
 
 
