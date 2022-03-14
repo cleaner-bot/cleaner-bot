1422e9d4d45629bb58bcaca85361431d0254838d
Leo Developer <git@leodev.xyz>
2022-03-14 15:19:21 +0100

    add permissions checks

--- clend/challenge/ext.py
+++ clend/challenge/ext.py
@@ -34,18 +34,19 @@ class ChallengeExtension:
 
     async def on_interaction_create(self, event: hikari.InteractionCreateEvent):
         interaction = event.interaction
-        if (
-            not isinstance(interaction, hikari.ComponentInteraction)
-            or interaction.guild_id is None
-        ):
+        if not isinstance(interaction, hikari.ComponentInteraction):
+            return
+        guild = interaction.get_guild()
+        if guild is None:
             return
 
         custom_id = interaction.custom_id.split("/")
         if custom_id[0] != "challenge":
             return
 
-        config = self.get_config(interaction.guild_id)
+        config = self.get_config(guild.id)
         if config is None:
+            logger.warning(f"uncached guild settings: {guild.id}")
             url = "https://cleaner.leodev.xyz/discord"
             component = event.app.rest.build_action_row()
             (
@@ -80,19 +81,98 @@ class ChallengeExtension:
                 hikari.ResponseType.MESSAGE_CREATE,
                 content=(
                     f"Server staff has not selected a role for me to {act}.\n"
-                    f"Contact server staff and inform them that they haven't "
+                    f"Contact server staff and inform them that they have not "
                     f"completed the setup yet."
                 ),
                 flags=hikari.MessageFlag.EPHEMERAL,
             )
 
-        role = config.challenge_interactive_role
         member = interaction.member
         if member is None or (
-            role not in member.role_ids == config.challenge_interactive_take_role
+            (config.challenge_interactive_role not in member.role_ids)
+            == config.challenge_interactive_take_role
         ):
             return await interaction.create_initial_response(
-                hikari.ResponseType.MESSAGE_CREATE, content="You are already verified."
+                hikari.ResponseType.MESSAGE_CREATE,
+                content="You are already verified.",
+                flags=hikari.MessageFlag.EPHEMERAL,
+            )
+
+        role = guild.get_role(config.challenge_interactive_role)
+        if role is None:
+            act = "take" if config.challenge_interactive_take_role else "give"
+            return await interaction.create_initial_response(
+                hikari.ResponseType.MESSAGE_CREATE,
+                content=(
+                    f"I can not find the role I am supposed to {act}. "
+                    f"Maybe it has been deleted?\n"
+                    f"Contact server staff and inform them to select an "
+                    f"up-to-date role."
+                ),
+            )
+        elif role.is_managed:
+            act = "take" if config.challenge_interactive_take_role else "give"
+            # TODO: make a help desk article
+            return await interaction.create_initial_response(
+                hikari.ResponseType.MESSAGE_CREATE,
+                content=(
+                    f"I can not {act} the role {role.mention} because it is "
+                    f"managed (e.g. a bot role, the server booster role, or "
+                    f"part of a different integration)"
+                ),
+                flags=hikari.MessageFlag.EPHEMERAL,
+            )
+
+        me = guild.get_my_member()
+        if me is None:
+            url = "https://cleaner.leodev.xyz/discord"
+            component = event.app.rest.build_action_row()
+            (
+                component.add_button(hikari.ButtonStyle.LINK, url)
+                .set_label("Support")
+                .add_to_container()
+            )
+
+            return await interaction.create_initial_response(
+                hikari.ResponseType.MESSAGE_CREATE,
+                content=(
+                    "**Internal error**: I can not find myself.\n"
+                    "Please try again in a few minutes.\n\n"
+                    "If this problem persists please contact support."
+                ),
+                component=component,
+                flags=hikari.MessageFlag.EPHEMERAL,
+            )
+
+        top_role = me.get_top_role()
+        if top_role is not None and role.position >= top_role.position:
+            act = "take" if config.challenge_interactive_take_role else "give"
+            # TODO: make a help desk article
+            return await interaction.create_initial_response(
+                hikari.ResponseType.MESSAGE_CREATE,
+                content=(
+                    f"The role I am supposed to {act} is above me in the role "
+                    f"hierarchy and therefore I can not give it.\n"
+                    f"Contact server staff and ask them to move me above the "
+                    f"{role.mention} role in the role settings."
+                ),
+                flags=hikari.MessageFlag.EPHEMERAL,
+            )
+
+        for my_role in me.get_roles():
+            if my_role.permissions & hikari.Permissions.ADMINISTRATOR:
+                break
+            elif my_role.permissions & hikari.Permissions.MANAGE_ROLES:
+                break
+        else:
+            return await interaction.create_initial_response(
+                hikari.ResponseType.MESSAGE_CREATE,
+                content=(
+                    "I do not have permission to give roles :(\n"
+                    "Contact server staff and ask them to give me the "
+                    "`ADMINISTRATOR` or `MANAGE ROLES` permission."
+                ),
+                flags=hikari.MessageFlag.EPHEMERAL,
             )
 
         flow = hashlib.sha256(interaction.id.to_bytes(8, "big")).hexdigest()
@@ -133,7 +213,6 @@ class ChallengeExtension:
         async for event in pubsub_listen(pubsub):
             if not isinstance(event, Message):
                 continue
-            print(event)
 
             flow = event.data.decode()
 
@@ -143,15 +222,34 @@ class ChallengeExtension:
             guild_id = await self.bot.database.get(f"challenge:flow:{flow}:guild")
 
             guild = self.bot.bot.cache.get_guild(int(guild_id))
+            if guild is None:
+                logger.warning(f"uncached guild: {guild_id}")
+                continue
             config = self.get_config(guild.id)
 
             if config is None:
                 logger.warning("uncached guild settings")
                 continue
-            elif not config.challenge_interactive_role:
-                logger.warning("role id 0 for interactive challenge")
+
+            role = guild.get_role(config.challenge_interactive_role)
+            if role is None or role.is_managed:
                 continue
-            # TODO: permissions checks
+
+            me = guild.get_my_member()
+            if me is None:
+                continue
+
+            top_role = me.get_top_role()
+            if top_role is not None and role.position >= top_role.position:
+                continue
+
+            for my_role in me.get_roles():
+                if my_role.permissions & hikari.Permissions.ADMINISTRATOR:
+                    break
+                elif my_role.permissions & hikari.Permissions.MANAGE_ROLES:
+                    break
+            else:
+                continue
 
             routine = self.bot.bot.rest.add_role_to_member
             if config.challenge_interactive_take_role:
