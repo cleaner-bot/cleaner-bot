8fd6e065e2d3301ca440771310c5dd916a7b4c2a
Leo Developer <leodeveloper@protonmail.com>
2022-04-11 16:41:56 +0200

    fix old_member being None sometimes

--- clend/guild/components/impersonation.py
+++ clend/guild/components/impersonation.py
@@ -16,7 +16,10 @@ def on_new_member(
     config = guild.get_config()
     if config is None or (
         isinstance(event, hikari.MemberUpdateEvent)  # prevent duplicate actions
-        and event.old_member.username == event.member.username
+        and (
+            event.old_member is None
+            or event.old_member.username == event.member.username
+        )
         and (event.member.joined_at - utc_datetime()).total_seconds() < 5
     ):
         return
