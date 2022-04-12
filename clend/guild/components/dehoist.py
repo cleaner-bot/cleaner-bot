5267678322b857131bedd52677a1ee5412b4e0a2
Leo Developer <leodeveloper@protonmail.com>
2022-04-12 12:52:43 +0200

    fix dehoist

--- clend/guild/components/dehoist.py
+++ clend/guild/components/dehoist.py
@@ -12,6 +12,7 @@ def on_member_update(event: hikari.MemberUpdateEvent, guild: CleanerGuild):
         config is None
         or not config.general_dehoisting_enabled
         or event.member.username == event.member.nickname
+        or event.member.nickname is None
         or not event.member.display_name.startswith("!")
         or is_moderator(guild, event.member)
     ):
@@ -21,7 +22,7 @@ def on_member_update(event: hikari.MemberUpdateEvent, guild: CleanerGuild):
     info = {
         "name": "dehoist",
         "username": event.member.username,
-        "nickanme": event.member.nickname,
+        "nickname": event.member.nickname,
     }
     return (action_nickname(event.member, reason=reason, info=info),)
 
