332d7159ee94994ea41dff996da33f0d8a038548
Leo Developer <leodeveloper@protonmail.com>
2022-04-12 10:41:18 +0200

    load env_bot too

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv(Path("~/.cleaner/secrets").expanduser())
 load_dotenv(Path("~/.cleaner/env").expanduser())
+load_dotenv(Path("~/.cleaner/env_bot").expanduser())
 
 from clend.bot import TheCleaner  # noqa: E402
 from pcex import inject  # noqa: E402
