5677223518182c7ec527b4caa6a64f3683b311a9
Leo Developer <leodeveloper@protonmail.com>
2022-07-21 20:14:07 +0200

    enhance phishing.embed detection

--- clend/guild/components/rules/phishing.py
+++ clend/guild/components/rules/phishing.py
@@ -1,11 +1,17 @@
 import hikari
 from cleaner_data.auto.phishing_embed_thumbnail import data as banned_thumbnails
 from cleaner_data.domains import is_domain_blacklisted, is_domain_whitelisted
+from cleaner_data.normalize import normalize
 from cleaner_data.phishing_content import get_highest_phishing_match
 from cleaner_data.url import get_urls, has_url
 from Levenshtein import ratio  # type: ignore
 
 from ...guild import CleanerGuild
+
+banned_descriptions = {
+    "and bigger discord discords e emoji enjoy fauorite file free from get in "
+    "months more nitro of out stand steam upgrade uploads your",
+}
 
 
 def phishing_content(message: hikari.PartialMessage, guild: CleanerGuild) -> bool:
@@ -60,14 +66,23 @@ def phishing_embed(message: hikari.PartialMessage, guild: CleanerGuild) -> bool:
     if not message.embeds:
         return False
     for embed in message.embeds:
-        url = embed.url
-        if url is None:
-            continue
-        hostname = url.split("/")[2]
-        if (
-            not is_domain_whitelisted(hostname)
-            and embed.thumbnail
-            and embed.thumbnail.url in banned_thumbnails
-        ):
+        if not is_embed_legitimate(embed) and is_embed_suspicious(embed):
             return True
+    return False
+
+
+def is_embed_legitimate(embed: hikari.Embed) -> bool:
+    if embed.url is None:
+        return True
+    hostname = embed.url.split("/")[2]
+    return is_domain_whitelisted(hostname)
+
+
+def is_embed_suspicious(embed: hikari.Embed) -> bool:
+    if embed.description and normalize(embed.description) in banned_descriptions:
+        return True
+
+    if embed.thumbnail and embed.thumbnail.url in banned_thumbnails:
+        return True
+
     return False
